707c38e42ffb8e311ba20695e08d3c59cec2313a
Eduardo de Almeida <edu_almeida3@hotmailc.com>
2025-10-27 21:42:15 -0300

    Corrigindo o Dependencia

--- vnet.tf
+++ vnet.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "private_endpoint_subnet" {
   address_prefixes     = ["192.168.3.0/24"]
 
   service_endpoints = ["Microsoft.Storage"]
-    depends_on = [
+  depends_on = [
     azurerm_private_endpoint.storage_pe
   ]
 }
@@ -35,6 +35,10 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
   private_dns_zone_name = azurerm_private_dns_zone.storage_dns.name
   virtual_network_id    = azurerm_virtual_network.vnet.id
   registration_enabled  = false
+  depends_on = [
+    azurerm_private_dns_zone.storage_dns,
+    azurerm_virtual_network.vnet
+  ]
 }
 
 # 🔹 Private Endpoint com associação DNS (nova sintaxe)
@@ -55,6 +59,11 @@ resource "azurerm_private_endpoint" "storage_pe" {
     name                 = "storage-dns-zone-group"
     private_dns_zone_ids = [azurerm_private_dns_zone.storage_dns.id]
   }
+  depends_on = [
+    azurerm_storage_account.storage,
+    azurerm_subnet.private_endpoint_subnet,
+    azurerm_private_dns_zone_virtual_network_link.dns_link
+  ]
 }
 
 resource "azurerm_virtual_network_peering" "storage_to_servidores" {
@@ -68,7 +77,7 @@ resource "azurerm_virtual_network_peering" "storage_to_servidores" {
   allow_virtual_network_access = true
 
   depends_on = [
-    azurerm_private_endpoint.storage_pe
+    azurerm_virtual_network.vnet
   ]
 }
 
@@ -84,7 +93,6 @@ resource "azurerm_virtual_network_peering" "servidores_to_storage" {
   allow_virtual_network_access = true
 
   depends_on = [
-    azurerm_virtual_network.vnet,
-    azurerm_subnet.private_endpoint_subnet
+    azurerm_virtual_network.vnet
   ]
 }
